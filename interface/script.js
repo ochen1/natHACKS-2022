4b026eab2596ae5efb4e45f4853b5d076010cdc1
wdotmathree <wdotmathree@gmail.com>
2022-07-30 14:12:26 -0600

    Add socket.io

---
+++ interface/script.js
@@ -0,0 +1,57 @@
+var chart = new Chart("chart", {
+    type: "line",
+    data: {
+        labels: [0],
+        datasets: [
+            {
+                data: [0],
+                borderColor: "white",
+                fill: false,
+                backgroundColor: "transparent",
+            },
+        ],
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Custom Chart Title',
+            fontColor: '#40f2f9',
+        },
+        legend: {
+            display: false,
+        },
+        animation: {
+            duration: 1000,
+        },
+        scales: {
+            xAxes: [
+                {
+                    display: false,
+                },
+            ],
+            yAxes: [
+                {
+                    ticks: {
+                        fontColor: '#40f2f9',
+                        min: 0,
+                        max: 1,
+                        stepSize: 0.1,
+                    }
+                }
+            ]
+        }
+    },
+});
+
+const socket = io("http://172.20.139.134:5000/");
+
+socket.on("plot", (data) => {
+    console.log(data);
+    chart.data.labels.push(chart.data.labels[chart.data.labels.length - 1] + 1);
+    chart.data.datasets[0].data.push(data);
+    if (chart.data.labels.length > 10) {
+        chart.data.labels.shift();
+        chart.data.datasets[0].data.shift();
+    }
+    chart.update();
+});
